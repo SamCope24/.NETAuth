f0a8e706692c1758f9cb08c9c87a24e13a1d492b
Sam <samcope24@gmail.com>
2022-05-20 21:51:13 +0100

    link api to postgres

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Auth.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 

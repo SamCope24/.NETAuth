cf3815d9a4d3e77df3f8e18e2dda5b2044f3437f
Sam <samcope24@gmail.com>
2022-05-22 15:22:44 +0100

    add JWT configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using Auth.Data;
 using Auth.Data.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +14,35 @@ builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// configure DbContext
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+// add Identify
 builder.Services.AddIdentity<ApplicationUserEntity, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
         .AddDefaultTokenProviders();
+
+// add authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+// add JWT bearer
+.AddJwtBearer(options => {
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["JWT:Secret"])),
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"]
+    };
+});
 
 var app = builder.Build();
 
@@ -26,6 +54,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// use Authentication + Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
